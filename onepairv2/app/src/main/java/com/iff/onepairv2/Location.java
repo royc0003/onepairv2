e6ebaec0c9de4ee6874a57539247aab51822edff
jozua12345 <jozua12345@outlook.com>
2020-03-06 12:58:35 +0800

    Add location

---
+++ onepairv2/app/src/main/java/com/iff/onepairv2/Location.java
@@ -0,0 +1,25 @@
+package com.iff.onepairv2;
+
+public class Location {
+    public static final CharSequence[] LOCATIONS = {"Admiralty", "Aljunied", "Ang Mo Kio", "Bakau", "Bangkit",
+        "Bartley", "Bayfront", "Beauty World", "Bedok", "Bedok North", "Bedok Reservoir", "Bencoolen", "Bendemeer",
+        "Bishan", "Boon Keng", "Boon Lay", "Botanic Gardens", "Braddell", "Bras Basah", "Buangkok", "Bugis",
+        "Bukit Batok", "Bukit Gombak", "Bukit Panjang", "Buona Vista", "Caldecott", "Cashew", "Changi Airport",
+        "Cheng Lim", "Chinatown", "Chinese Garden", "Choa Chu Kang", "City Hall", "Clarke Quay", "Clementi",
+        "Commonwealth", "Compassvale", "Coral Edge", "Cove", "Dakota", "Damai", "Dhoby Ghaut", "Dover", "Downtown",
+        "Esplanade", "Eunos", "Expo", "Fajar", "Farmway", "Farrer Park", "Farrer Road", "Fernvale", "Fort Canning",
+        "Geylang Bahru", "Gul Circle", "HarbourFront", "Haw Par Villa", "Hillview", "Holland Village", "Hougang",
+        "Jalan Besar", "Jelapang", "Joo Koon", "Jurong East", "Kadaloor", "Kaki Bukit", "Kallang", "Kangkar",
+        "Keat Hong", "Kembangan", "Kent Ridge", "Khatib", "King Albert Park", "Kovan", "Kranji", "Kupang",
+        "Labrador Park", "Lakeside", "Lavender", "Layar", "Little India", "Lorong Chuan", "MacPherson",
+        "Marina Bay", "Marina South Pier", "Marsiling", "Marymount", "Mattar", "Meridian", "Mountbatten", "Newton",
+        "Nibong", "Nicoll Highway", "Novena", "Oasis", "one-north", "Orchard", "Outram Park", "Pasir Panjang",
+        "Pasir Ris", "Paya Lebar", "Pending", "Petir", "Phoenix", "Pioneer", "Potong Pasir", "Promenade", "Punggol",
+        "Punggol Point", "Queenstown", "Raffles Place", "Ranggung", "Redhill", "Renjong", "Riviera", "Rochor",
+        "Rumbia", "Sam Kee", "Samudera", "Segar", "Sembawang", "Sengkang", "Senja", "Serangoon", "Simei",
+        "Sixth Avenue", "Somerset", "Soo Teck", "South View", "Stadium", "Stevens", "Sumang",
+        "Tai Seng", "Tampines", "Tampines East", "Tampines West", "Tan Kah Kee", "Tanah Merah", "Tanjong Pagar",
+        "Teck Lee", "Teck Whye", "Telok Ayer", "Telok Blangah", "Ten Mile Junction", "Thanggam", "Tiong Bahru",
+        "Toa Payoh", "Tongkang", "Tuas Crescent", "Tuas Link", "Tuas West Road", "Ubi", "Upper Changi", "Woodlands",
+        "Woodleigh", "Yew Tee", "Yio Chu Kang", "Yishun"};
+}
