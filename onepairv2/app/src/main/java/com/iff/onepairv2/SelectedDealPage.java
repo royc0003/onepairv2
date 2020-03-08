348b07c4929a60527700741033a0cc3e56a107f9
Royce <royceang96@gmail.com>
2020-03-08 15:18:37 +0800

    Added some comments

--- onepairv2/app/src/main/java/com/iff/onepairv2/SelectedDealPage.java
+++ onepairv2/app/src/main/java/com/iff/onepairv2/SelectedDealPage.java
@@ -272,6 +272,39 @@ public class SelectedDealPage extends AppCompatActivity {
     public String name;
     public String uid;
     public String image;    *
+
+        @GET("getAllDeals")
+    Call<ArrayList<Deal>> getAllDeals();
+
+    @GET("getFoodDeals")
+    Call<ArrayList<Deal>> getFoodDeals();
+
+    @GET("getEntertainmentDeals")
+    Call<ArrayList<Deal>> getEntertainmentDeals();
+
+    @GET("getRetailDeals")
+    Call<ArrayList<Deal>> getRetailDeals();
+
+    @GET("getOthersDeals")
+    Call<ArrayList<Deal>> getOthersDeals();
+
+    @GET("addUser/{uid}")
+    Call<Void> addUser(@Path("uid") String uid);
+
+    @GET("addBlacklist/{dealid}/{uid1}/{uid2}")
+    Call<Void> addBlacklist(@Path("dealid") int dealid, @Path("uid1") String uid1, @Path("uid2") String uid2);
+
+    @GET("addRequest/{uid}/{dealid}/{c}")
+    Call<Void> addRequest(@Path("uid") String uid, @Path("dealid") int dealid, @Path("c") String c);
+
+    @GET("deleteRequest/{uid}/{dealid}")
+    Call<Void> deleteRequest(@Path("uid") String uid, @Path("dealid") int dealid);
+
+    @GET("getAllLocation")
+    Call<ArrayList<Location>> getAllLocation();
+
+    @GET("updateToken/{uid}/{token}")
+    Call<Void> updateToken(@Path("uid") String uid, @Path("token") String token);
     */
 
 
